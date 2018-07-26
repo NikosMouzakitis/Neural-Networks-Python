f74bc98459a81cfc9ce7768219bd0eb72fb93630
NikosMouzakitis <mzktsn@gmail.com>
2018-07-26 19:58:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+w = 5 
+b1 = -8.0
+b2 = 0.0
+b3 = 8.0
+
+l1 = 'b = -8.0'
+l2 = 'b = 0.0'
+l3 = 'b = 8.0'
+
+x = np.arange( -10, 10, 0.1)
+
+for b, l in [ (b1, l1) , (b2, l2) , (b3, l3) ]:
+	f = 1 / (1 + np.exp(-(x*w+b)))
+	plt.plot(x, f, label=l)
+
+plt.xlabel('x')
+plt.ylabel('h_wb(x)')
+plt.legend(loc=2)
+plt.show()
